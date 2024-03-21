aaa5368338c6b3e1c29c0d64f94c83ddc2e94572
Simon <ximeng.liu@anu.edu.au>
2024-03-22 00:52:28 +1100

    added a comment to finish tomorrow

--- 345_reverse_vowels_of_a_string.java
+++ 345_reverse_vowels_of_a_string.java
@@ -1,27 +1,29 @@
+// Original solution, really stupid
 class Solution {
-    public String reverseVowels(String s) {
-        StringBuilder vowels = new StringBuilder();
-        char[] chr = s.toCharArray();
-        for (int i = chr.length - 1; i >= 0; i--) {
-            if ((chr[i] == 'a') || (chr[i] == 'e') || (chr[i] == 'i') || (chr[i] == 'o') || (chr[i] == 'u')
-            || (chr[i] == 'A') || (chr[i] == 'E') || (chr[i] == 'I') || (chr[i] == 'O') || (chr[i] == 'U')) {
+  public String reverseVowels(String s) {
+    StringBuilder vowels = new StringBuilder();
+    char[] chr = s.toCharArray();
+    for (int i = chr.length - 1; i >= 0; i--) {
+      if ((chr[i] == 'a') || (chr[i] == 'e') || (chr[i] == 'i') || (chr[i] == 'o') || (chr[i] == 'u')
+          || (chr[i] == 'A') || (chr[i] == 'E') || (chr[i] == 'I') || (chr[i] == 'O') || (chr[i] == 'U')) {
 
-                vowels.append(chr[i]);
-            }
-        }
-
-        int index = 0;
-        String vowelString = vowels.toString();
-
-        for (int i = 0; i < chr.length; i++) {
-            if ((chr[i] == 'a') || (chr[i] == 'e') || (chr[i] == 'i') || (chr[i] == 'o') || (chr[i] == 'u')
-                    || (chr[i] == 'A') || (chr[i] == 'E') || (chr[i] == 'I') || (chr[i] == 'O') || (chr[i] == 'U')) {
-                chr[i] = vowels.charAt(index);
-                index++;
-            }
-        }
-        String ret = new String(chr);
-        return ret;
+        vowels.append(chr[i]);
+      }
     }
+
+    int index = 0;
+    String vowelString = vowels.toString();
+
+    for (int i = 0; i < chr.length; i++) {
+      if ((chr[i] == 'a') || (chr[i] == 'e') || (chr[i] == 'i') || (chr[i] == 'o') || (chr[i] == 'u')
+          || (chr[i] == 'A') || (chr[i] == 'E') || (chr[i] == 'I') || (chr[i] == 'O') || (chr[i] == 'U')) {
+        chr[i] = vowels.charAt(index);
+        index++;
+      }
+    }
+    String ret = new String(chr);
+    return ret;
+  }
 }
 
+// Using double pointers
