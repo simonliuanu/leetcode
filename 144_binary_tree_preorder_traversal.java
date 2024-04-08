b430eb938e80bbae4f8deb6930ad825623e6df2d
simon liu <real.lxm@gmail.com>
2024-04-08 16:37:34 +1000

    added recursion solution for problem 144

--- 144_binary_tree_preorder_traversal.java
+++ 144_binary_tree_preorder_traversal.java
@@ -1,1 +1,44 @@
+import java.util.LinkedList;
+import java.util.List;
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+
+// Using recursion
+class Solution {
+    public List<Integer> list;
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        list = new LinkedList<>();
+
+        if (root == null)
+            return list;
+
+        preorderRecur(root);
+
+        return list;
+    }
+
+    public void preorderRecur(TreeNode node) {
+        if (node == null)
+            return;
+
+        list.add(node.val);
+
+        preorderRecur(node.left);
+        preorderRecur(node.right);
+    }
+}
