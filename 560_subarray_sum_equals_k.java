cb75f01dac35d1e65df892df869d2b8c8af75277
simon liu <real.lxm@gmail.com>
2024-04-11 13:29:41 +1000

    added basic solution for problem 560

---
+++ 560_subarray_sum_equals_k.java
@@ -0,0 +1,15 @@
+// Enumerate results
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int cnt = 0;
+        for (int start = 0; start < nums.length; start++) {
+            int sum = 0;
+            for (int end = start; end < nums.length; end++) {
+                sum += nums[end];
+                if (sum == k)
+                    cnt++;
+            }
+        }
+        return cnt;
+    }
+}
