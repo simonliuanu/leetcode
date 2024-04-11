bcfd83c896a1831fca25a81424251a83ba94a0a0
simon liu <real.lxm@gmail.com>
2024-04-11 10:56:00 +1000

    added solution for problem 3

--- 3_longest_substring_without_repeating_characters.java
+++ 3_longest_substring_without_repeating_characters.java
@@ -1,3 +1,5 @@
+import java.util.Set;
+
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         Set<Character> ooc = new HashSet<>();
@@ -6,7 +8,8 @@ class Solution {
         int max = 0;
 
         for (int i = 0; i < s.length(); i++) {
-            if (i != 0) ooc.remove(s.charAt(i - 1));
+            if (i != 0)
+                ooc.remove(s.charAt(i - 1));
             while (r + 1 < s.length() && !ooc.contains(s.charAt(r + 1))) {
                 ooc.add(s.charAt(r + 1));
                 r++;
