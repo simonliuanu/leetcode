7c81bc23e08fa63eb29dacce6d624819235b8076
Simon <ximeng.liu@anu.edu.au>
2024-04-14 01:06:19 +1000

    added iteration solution for problem 206

--- 206_reverse_linked_list.java
+++ 206_reverse_linked_list.java
@@ -1,5 +1,14 @@
+// Iteration solution
 class Solution {
     public ListNode reverseList(ListNode head) {
-
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
     }
 }
