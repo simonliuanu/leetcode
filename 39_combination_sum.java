dd65300930ab31bb702bbdc55078d99a59e25d1b
Simon <ximeng.liu@anu.edu.au>
2024-04-22 00:36:51 +1000

    added backtracking solution for problem 39

--- 39_combination_sum.java
+++ 39_combination_sum.java
@@ -1,5 +1,26 @@
 class Solution {
-    public <List<List<Integer>> combinationSum(int[] candidates, int target) {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
+        List<Integer> combine = new ArrayList<Integer>();
+        dfs(candidates, target, ret, combine, 0);
+        return ret;
+    }
 
+    public void dfs(int[] candidates, int target, List<List<Integer>> ret, List<Integer> combine, int index) {
+        if (index == candidates.length) {
+            return;
+        }
+        if (target == 0) {
+            ret.add(new ArrayList<Integer>(combine));
+            return;
+        }
+
+        dfs(candidates, target, ret, combine, index + 1);
+
+        if (target - candidates[index] >= 0) {
+            combine.add(candidates[index]);
+            dfs(candidates, target - candidates[index], ret, combine, index);
+            combine.remove(combine.size() - 1);
+        }
     }
 }
