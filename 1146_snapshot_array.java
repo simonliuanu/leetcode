a44fc5265471bdd1dbe79a3e7e53bc6c4c5250ec
simon liu <real.lxm@gmail.com>
2024-04-26 17:19:15 +1000

    added solution of problem 1146

--- 1146_snapshot_array.java
+++ 1146_snapshot_array.java
@@ -1,17 +1,27 @@
+import java.util.TreeMap;
+
 class SnapshotArray {
     int snap_cnt = 0;
     TreeMap<Integer, Integer>[] historyRecords;
 
     public SnapshotArray(int length) {
+        historyRecords = new TreeMap[length];
+        for (int i = 0; i < length; i++) {
+            historyRecords[i] = new TreeMap<>();
+            historyRecords[i].put(0, 0);
+        }
     }
 
     public void set(int index, int val) {
+        historyRecords[index].push(snap_id, val);
     }
 
     public int snap() {
+        return snap_cnt++;
     }
 
     public int get(int index, int snap_id) {
+        return historyRecords[index].floorEntry(snap_cnt).getValue();
     }
 }
 
