151e311fcb39007bb88b1a79181ab39fd371077d
simon liu <real.lxm@gmail.com>
2024-05-01 15:17:38 +1000

    added solution for question 1046

---
+++ data structures/heap/1046_last_stone_weight.java
@@ -0,0 +1,19 @@
+// Official solution
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        for (int stone : stones) {
+            pq.offer(stone);
+        }
+
+        while (pq.size() > 1) {
+            int a = pq.poll();
+            int b = pq.poll();
+            if (a > b) {
+                pq.offer(a - b);
+            }
+        }
+
+        return pq.isEmpty() ? 0 : pq.poll();
+    }
+}
