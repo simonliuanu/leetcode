cd4285ee3156fabfd4c9e10b790e63901c9d3b2c
Simon <ximeng.liu@anu.edu.au>
2024-05-05 15:26:59 +1000

    added sliding window solution reaching O(n) on question 1652

--- daily challenge/2024/May/5th_1652_defuse_the_bomb.java
+++ daily challenge/2024/May/5th_1652_defuse_the_bomb.java
@@ -24,3 +24,45 @@ class Solution {
         return codeM;
     }
 }
+
+// Sliding window
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int n = code.length;
+        int[] ret = new int[n];
+
+        if (k == 0) return ret;
+
+        if (k > 0) {
+            int start = 1 % n;
+            // int r = (1 + k) % n;
+            int sum = 0;
+            for (int i = 0; i < k; i++) {
+                sum += code[(i + start) % n];
+            }
+            ret[0] = sum;
+            for (int i = 1; i < n; i++) {
+                sum -= code[i];
+                sum += code[(i + k) % n];
+                ret[i] = sum;
+            }
+        }
+
+        if (k < 0) {
+            int start = (-1 + n) % n;
+            int sum = 0;
+            for (int i = 0; i > k; i--) {
+                sum += code[((start + i) % n + n) % n];
+            }
+            ret[0] = sum;
+            for (int i = 1; i < n; i++) {
+                sum += code[i - 1];
+                sum -= code[((i - 1 + k) % n + n) % n];
+                ret[i] = sum;
+            }
+        }
+
+        return ret;
+    }
+}
+// Sliding window
