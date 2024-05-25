fafb4676f96ae416a0a95aa034e6183f64524c57
Simon <ximeng.liu@anu.edu.au>
2024-05-25 17:06:01 +1000

    added one time traversal solution for question 2903

--- daily challenge/2024/May/25th_2903_fidn_indices_with_index_and_value_difference_I.java
+++ daily challenge/2024/May/25th_2903_fidn_indices_with_index_and_value_difference_I.java
@@ -12,3 +12,22 @@ class Solution{
         return new int[]{-1, -1};
     }
 }
+
+// One traversal solution
+class Solution {
+    public int[] findIndices(int[] nums, int indexDifference, int valueDifference) {
+        int minIndex = 0, maxIndex = 0;
+        for (int j = indexDifference; j < nums.length; j++) {
+            int i = j - indexDifference;
+            if (nums[i] < nums[minIndex]) {
+                minIndex = i;
+            }
+            if (nums[j] - nums[minIndex] >= valueDifference) return new int[]{minIndex, j};
+            if (nums[i] > nums[maxIndex]) {
+                maxIndex = i;
+            }
+            if (nums[maxIndex] - nums[j] >= valueDifference) return new int[]{j, maxIndex};
+        }
+        return new int[]{-1, -1};
+    }
+}
