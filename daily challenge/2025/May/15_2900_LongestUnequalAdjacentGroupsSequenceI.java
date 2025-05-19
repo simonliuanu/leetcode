4b3991ef3ca9f94a2b50aab90e189e2e6314e24b
simon liu <ximeng.liu@anu.edu.au>
2025-05-20 00:54:21 +1000

    added solution for 2900

---
+++ daily challenge/2025/May/15_2900_LongestUnequalAdjacentGroupsSequenceI.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<String> getLongestSubsequence(String[] words, int[] groups) {
+        List<String> ret = new ArrayList<>();
+        int current = 0;
+        ret.add(words[0]);
+        for (int i = 1; i < groups.length; i++) {
+            if (groups[current] != groups[i]) {
+                ret.add(words[i]);
+                current = i;
+            }
+        }
+        return ret;
+    }
+}
