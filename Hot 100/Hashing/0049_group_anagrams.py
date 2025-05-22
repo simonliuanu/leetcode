2cb7c629217ddb8e43a3bde4288cb12acfbad137
simon liu <ximeng.liu@anu.edu.au>
2025-05-22 16:39:00 +1000

    updated solution for 0049

--- Hot 100/Hashing/0049_group_anagrams.py
+++ Hot 100/Hashing/0049_group_anagrams.py
@@ -1,11 +1,8 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-
         buckets: defaultdict[str, List[str]] = defaultdict(list);
-
         for s in strs:
             key = ''.join(sorted(s));
             buckets[key].append(s);
-
         return list(buckets.values());
 
