6d85f2641e9ee4d289b98a73aa0e23fe63251aed
Simon <ximeng.liu@anu.edu.au>
2025-05-26 02:30:07 +1000

    Add two solutions for 0560

--- Hot 100/Subarray/0560_SubarraySumEqualsK.java
+++ Hot 100/Subarray/0560_SubarraySumEqualsK.java
@@ -1,5 +1,32 @@
+// Two Traversal:
 class Solution {
     public int subarraySum(int[] nums, int k) {
+        int[] sums = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            sums[i + 1] = sums[i] + nums[i];
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        int ans = 0;
+        for (int sum : sums) {
+            ans += map.getOrDefault(sum - k, 0);
+            map.merge(sum, 1, Integer::sum);
+        }
+        return ans;
+    }
+}
 
+// One Traversal:
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int sum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        int ans = 0;
+        map.put(0, 1);
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            ans += map.getOrDefault(sum - k, 0);
+            map.merge(sum, 1, Integer::sum);
+        }
+        return ans;
     }
 }
