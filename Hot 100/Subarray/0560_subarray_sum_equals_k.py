bc2b12689b108804f2cb93e4c251595beb423ca9
Simon <ximeng.liu@anu.edu.au>
2025-05-26 11:22:49 +1000

    Add solution for 0560 in Python

---
+++ Hot 100/Subarray/0560_subarray_sum_equals_k.py
@@ -0,0 +1,27 @@
+# Two Traversal
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        occ: dict[int, int] = {}
+        ans = 0
+        n = len(nums)
+        sums = [0] * (n + 1)
+        for i in range(n):
+            sums[i + 1] = nums[i] + sums[i]
+        for i in range(n + 1):
+            ans = ans + occ.get(sums[i] - k, 0)
+            occ[sums[i]] = occ.get(sums[i], 0) + 1
+        return ans
+
+# One Traversal
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        ans = 0
+        occ: dict[int, int] = {}
+        n = len(nums)
+        sums = 0
+        occ[0] = 1
+        for i in range(n):
+            sums = sums + nums[i]
+            ans = ans + occ.get(sums - k, 0)
+            occ[sums] = occ.get(sums , 0) + 1
+        return ans
