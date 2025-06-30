2061eaa92bfc84d01a31f6f0b9cec1fe413769c0
Simon <ximeng.liu@anu.edu.au>
2025-06-30 15:28:00 +1000

    Add solutino for dailychallenge 3330

---
+++ daily challenge/2025/Jul/01_3330_FindTheOriginalTypedStringI.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int possibleStringCount(String word) {
+        int ans = 0;
+        for (int i = 1; i < word.length(); i++) {
+            if (word.charAt(i) == word.charAt(i - 1)) ans++;
+        }
+
+        return ans + 1;
+    }
+}
