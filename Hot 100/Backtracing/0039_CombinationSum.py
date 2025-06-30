30687ccf765395a05f4f442f5386769857653433
Simon <ximeng.liu@anu.edu.au>
2025-06-30 17:01:44 +1000

    Add solution of 0039 in Python3

--- Hot 100/Backtracing/0039_CombinationSum.py
+++ Hot 100/Backtracing/0039_CombinationSum.py
@@ -1,3 +1,23 @@
+//Backtracing
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        ans = []
 
+        def backtrack(start: int, remain: int, path: List[int]):
+            if remain == 0:
+                ans.append(list(path))
+                return
+            if remain < 0:
+                return
+            for i in range(start, len(candidates)):
+                pick = candidates[i]
+                if pick > remain:
+                    return
+                path.append(pick)
+                backtrack(i, remain - pick, path)
+                path.pop()
+
+        backtrack(0, target, [])
+
+        return ans
