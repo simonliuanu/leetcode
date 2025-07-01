8132fe7759de8cfd68fcc8a516a5b11a41fef2f2
Simon <ximeng.liu@anu.edu.au>
2025-07-02 04:53:22 +1000

    Update an optimized solution of 0131

--- Hot 100/Backtracing/0131_palindrome_partitioning.py
+++ Hot 100/Backtracing/0131_palindrome_partitioning.py
@@ -16,3 +16,33 @@ class Solution:
 
         dfs(0)
         return ans
+
+# Constructing a Palindrome Table First:
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        n = len(s)
+        isPal = [[False] * n for _ in range(n)]
+
+        for i in range(n):
+            isPal[i][i] = True
+        for L in range(2, n + 1):
+            for i in range(n - L + 1):
+                j = i + L - 1
+                if s[i] == s[j] and (L == 2 or isPal[i + 1][j - 1]):
+                    isPal[i][j] = True
+
+        ans = []
+        path = []
+
+        def dfs(start: int):
+            if start == n:
+                ans.append(path.copy())
+                return True
+            for end in range(start, n):
+                if isPal[start][end]:
+                    path.append(s[start:end + 1])
+                    dfs(end + 1)
+                    path.pop()
+
+        dfs(0)
+        return ans
