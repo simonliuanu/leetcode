df76cfefecf6c8820bdd678ea246e33ec1fe0849
Simon <ximeng.liu@anu.edu.au>
2025-07-02 21:03:27 +1000

    Add solution of 0234 palindrome linked list

--- Hot 100/Linked Lists/0234_palindrome_linked_list.py
+++ Hot 100/Linked Lists/0234_palindrome_linked_list.py
@@ -5,3 +5,29 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        def middleNode(head: Optional[ListNode]) -> Optional[ListNode]:
+            slow, fast = head, head
+            while fast and fast.next:
+                slow = slow.next
+                fast = fast.next.next
+            return slow
+
+        def reverseList(head: Optional[ListNode]) -> Optional[ListNode]:
+            pre, cur = None, head
+            while cur:
+                tmp = cur.next
+                cur.next = pre
+                pre = cur
+                cur = tmp
+            return pre
+
+        mid = middleNode(head)
+        head2 = reverseList(mid)
+
+        while head2:
+            if head.val != head2.val:
+                return False
+            head = head.next
+            head2 = head2.next
+        return True
+
