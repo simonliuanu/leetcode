89e9d2264cc6bebda8d2f97409d426877fd71671
Simon <ximeng.liu@anu.edu.au>
2025-07-04 18:55:18 +1000

    Update iteration solution for 0021

--- Hot 100/Linked Lists/0021_merge_two_sorted_lists.py
+++ Hot 100/Linked Lists/0021_merge_two_sorted_lists.py
@@ -3,6 +3,8 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# Recursion
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
       if not list1: return list2
@@ -15,3 +17,21 @@ class Solution:
         list2.next = self.mergeTwoLists(list1, list2.next)
         return list2
 
+# Iteration
+class Solution:
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        tail = dummy
+
+        while l1 and l2:
+            if l1.val <= l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+
+        tail.next = l1 if l1 else l2
+
+        return dummy.next
