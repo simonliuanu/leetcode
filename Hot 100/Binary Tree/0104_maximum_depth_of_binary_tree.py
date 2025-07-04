caf9c70a660fb13c2afa223afe9e3234a02ac27f
Simon <ximeng.liu@anu.edu.au>
2025-07-04 23:38:57 +1000

    Add DFS & BFS solution of 0104

---
+++ Hot 100/Binary Tree/0104_maximum_depth_of_binary_tree.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# DFS
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root: return 0
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+
+# BFS
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root: return 0
+        ans = 0
+        queue = [root]
+        while queue:
+            tmp = []
+            for node in queue:
+                if node.left: tmp.append(node.left)
+                if node.right: tmp.append(node.right)
+            queue = tmp
+            ans += 1
+        return ans
+
