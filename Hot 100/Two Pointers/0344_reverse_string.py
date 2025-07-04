b2ae14c88d3ff53d8b5647ea325f306544e979c1
Simon <ximeng.liu@anu.edu.au>
2025-07-04 19:06:40 +1000

    Update an optimised solution of 0344

--- Hot 100/Two Pointers/0344_reverse_string.py
+++ Hot 100/Two Pointers/0344_reverse_string.py
@@ -12,3 +12,12 @@ class Solution:
           s[j] = tmp
           i += 1
           j -= 1
+
+# Optimised
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        i, j = 0, len(s) - 1
+        while i < j:
+            s[i], s[j] = s[j] , s[i]
+            i += 1
+            j -= 1
