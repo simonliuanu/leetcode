070465e28b75d6343e3bfd7c7e28395f9967d87d
Simon <ximeng.liu@anu.edu.au>
2025-07-04 19:06:27 +1000

    Add solution of 0344

---
+++ Hot 100/Two Pointers/0344_reverse_string.py
@@ -0,0 +1,14 @@
+# self-solution
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+
+        i, j = 0, len(s) - 1
+        while i < j:
+          tmp = s[i]
+          s[i] = s[j]
+          s[j] = tmp
+          i += 1
+          j -= 1
