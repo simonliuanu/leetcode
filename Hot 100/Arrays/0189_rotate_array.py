a06b78d8f2f086278b79e5202f3f48a764cb2146
Simon <ximeng.liu@anu.edu.au>
2025-07-10 01:08:29 +1000

    Update 0189 with O(1) solution

--- Hot 100/Arrays/0189_rotate_array.py
+++ Hot 100/Arrays/0189_rotate_array.py
@@ -11,3 +11,14 @@ class Solution:
         for i in range(n):
             ans[(i + k) % n] = nums[i]
         nums[:] = ans
+
+# O(1) in-place solution:
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k %= len(nums)
+        nums.reverse()
+        nums[:k] = reversed(nums[:k])
+        nums[k:] = reversed(nums[k:])
