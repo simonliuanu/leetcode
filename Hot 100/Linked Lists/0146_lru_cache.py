f59d2b4ba985388903bb80b53e687a7b0a14d581
simon liu <ximeng.liu@anu.edu.au>
2025-07-09 20:05:56 +1000

    Halfway commit

--- Hot 100/Linked Lists/0146_lru_cache.py
+++ Hot 100/Linked Lists/0146_lru_cache.py
@@ -1,12 +1,44 @@
+class Node:
+    __slots__ = "prev", "next", "key", "value"
+
+    def __init__(self, key = 0, value = 0) -> None:
+        self.key = key
+        self.value = value
+
 class LRUCache:
 
     def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.dummy = Node()
+        self.dummy.next = dummy
+        self.dummy.prev = dummy
+        self.dic = {}
 
+    def get_node(self, key: int) -> Optional[Node]:
+        if key not in self.dic:
+            return None
+        else:
+            node = self.dic[key]
+            self.remove(node)
+            self.push(node)
+            return node
+
+    def remove(self, node: Node) -> None:
+        node.next.prev = node.prev
+        node.prev.next = node.next
+
+    def push(self, node: Node) -> None:
+        node.prev = dummy
+        node.next = dummy.next
+        node.next.prev = node
+        node.prev.next = node
 
     def get(self, key: int) -> int:
-
+        node = self.dic[key]
+        return node.val if node else -1
 
     def put(self, key: int, value: int) -> None:
+
 
 
 
