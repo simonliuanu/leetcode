3e7bf4c6f980e540340d5de4b787848dc58f385f
Simon <ximeng.liu@anu.edu.au>
2025-07-09 13:00:21 +1000

    Add solution for 0146

--- Hot 100/Linked Lists/0146_lru_cache.py
+++ Hot 100/Linked Lists/0146_lru_cache.py
@@ -10,8 +10,8 @@ class LRUCache:
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.dummy = Node()
-        self.dummy.next = dummy
-        self.dummy.prev = dummy
+        self.dummy.next = self.dummy
+        self.dummy.prev = self.dummy
         self.dic = {}
 
     def get_node(self, key: int) -> Optional[Node]:
@@ -28,22 +28,29 @@ class LRUCache:
         node.prev.next = node.next
 
     def push(self, node: Node) -> None:
-        node.prev = dummy
-        node.next = dummy.next
+        node.prev = self.dummy
+        node.next = self.dummy.next
         node.next.prev = node
         node.prev.next = node
 
     def get(self, key: int) -> int:
-        node = self.dic[key]
-        return node.val if node else -1
+        node = self.get_node(key)
+        return node.value if node else -1
 
     def put(self, key: int, value: int) -> None:
-
-
+        node = self.get_node(key)
+        if node:
+            node.value = value
+            return
+        else:
+            self.dic[key] = new_node = Node(key, value)
+            self.push(new_node)
+            if len(self.dic) > self.capacity:
+                del self.dic[self.dummy.prev.key]
+                self.remove(self.dummy.prev)
 
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
 # obj.put(key,value)
-
