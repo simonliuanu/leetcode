57079785b950f29980462110a376b444773ddca0
simon liu <ximeng.liu@anu.edu.au>
2025-07-09 19:25:12 +1000

    Update optimisation for 0023

--- Hot 100/Linked Lists/0023_merge_k_sorted_lists.py
+++ Hot 100/Linked Lists/0023_merge_k_sorted_lists.py
@@ -28,3 +28,34 @@ class Solution:
         for i in range(1, len(lists)):
             merged = mergeList(merged, lists[i])
         return merged
+
+# optimisation:
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists:
+            return None
+
+        def mergeList(l, r):
+            dummy = ListNode()
+            cur = dummy
+            while l and r:
+                if l.val < r.val:
+                    cur.next = l
+                    l = l.next
+                else:
+                    cur.next = r
+                    r = r.next
+                cur = cur.next
+            cur.next = l if l else r
+            return dummy.next
+
+        def helper(l, r):
+            if l == r:
+                return lists[l]
+            mid = (l + r) >> 1
+            left = helper(l, mid)
+            right = helper(mid + 1, r)
+
+            return mergeList(left, right)
+
+        return helper(0, len(lists) - 1)
