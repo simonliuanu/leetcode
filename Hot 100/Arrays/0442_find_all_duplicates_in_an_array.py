61eb47befc1f4b3c792116fea0958e1139b06a2b
Simon <ximeng.liu@anu.edu.au>
2025-07-11 05:19:18 +1000

    Update 0042 with another Python solution

--- Hot 100/Arrays/0442_find_all_duplicates_in_an_array.py
+++ Hot 100/Arrays/0442_find_all_duplicates_in_an_array.py
@@ -9,3 +9,16 @@ class Solution:
             if nums[i] != i + 1:
                 ans.append(nums[i])
         return ans
+
+# another in-place solution:
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        ans = []
+        for i in range(n):
+            tmp = abs(nums[i])
+            if nums[tmp - 1] > 0:
+                nums[tmp - 1] *= -1
+            else:
+                ans.append(tmp)
+        return ans
