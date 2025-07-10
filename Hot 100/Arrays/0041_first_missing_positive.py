fcf833ff41765df32931fe70289f70bc189e6824
Simon <ximeng.liu@anu.edu.au>
2025-07-11 00:03:22 +1000

    Update binary search solution for 0041

--- Hot 100/Arrays/0041_first_missing_positive.py
+++ Hot 100/Arrays/0041_first_missing_positive.py
@@ -9,3 +9,29 @@ class Solution:
             if i not in s:
                 return i
         return n + 1
+
+# binary search solution(O(n) time complexity not satisfied):
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+
+        def binary_search(nums: List[int], i: int) -> int:
+            n = len(nums)
+            l, r = 0, n - 1
+            while l < r:
+                m = (l + r) >> 1
+                if nums[m] == i:
+                    return m
+                elif nums[m] < i:
+                    l = m + 1
+                else:
+                    r = m - 1
+            return -1
+
+        n = len(nums)
+        nums.sort()
+        for i in range(1, n + 1):
+            res = binary_search(nums, i)
+            if res == -1:
+                return i
+        return n + 1
+
