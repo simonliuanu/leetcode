9be7038783cf076547524d250636ed16bf796f1b
Simon <ximeng.liu@anu.edu.au>
2025-07-11 05:12:20 +1000

    Update 0442 java solution with in-place hashing

--- Hot 100/Arrays/0442_find_all_duplicates_in_an_array.java
+++ Hot 100/Arrays/0442_find_all_duplicates_in_an_array.java
@@ -1,4 +1,4 @@
-// hashset solution
+// hashset solution:
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
         Set<Integer> hashset = new HashSet<>();
@@ -15,4 +15,27 @@ class Solution {
     }
 }
 
+// in-place hashing solution:
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        int n = nums.length;
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            while (nums[i] != nums[nums[i] - 1]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                ans.add(nums[i]);
+            }
+        }
+        return ans;
+    }
 
+    private void swap(int[] nums, int a, int b) {
+        int tmp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = tmp;
+    }
+}
