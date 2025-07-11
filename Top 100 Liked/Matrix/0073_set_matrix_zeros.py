a4a49858621d20f05cd3f2cb7da9f73c4056ce88
Simon <ximeng.liu@anu.edu.au>
2025-07-11 20:36:43 +1000

    Update 0073 with optimised solution

--- Top 100 Liked/Matrix/0073_set_matrix_zeros.py
+++ Top 100 Liked/Matrix/0073_set_matrix_zeros.py
@@ -48,3 +48,30 @@ class Solution:
             for i in range(m):
                 matrix[i][0] = 0
 
+# optimised in-place solution:
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        row, col = len(matrix), len(matrix[0])
+        cor0 = False
+        for i in range(row):
+            if matrix[i][0] == 0:
+                cor0 = True
+                break
+        for i in range(row):
+            for j in range(1, col):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+        for i in range(1, row):
+            for j in range(1, col):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+        if matrix[0][0] == 0:
+            for i in range(col):
+                matrix[0][i] = 0
+        if cor0:
+            for i in range(row):
+                matrix[i][0] = 0
