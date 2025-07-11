4bc1f0efb646426c55746417675460bca5a1d63e
Simon <ximeng.liu@anu.edu.au>
2025-07-12 07:01:53 +1000

    Add solution for 0240

---
+++ Top 100 Liked/Matrix/0240_search_a_2d_matrix_ii.py
@@ -0,0 +1,23 @@
+# stupid solution(O(m * n)):
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m, n = len(matrix), len(matrix[0])
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == target:
+                    return True
+        return False
+
+# optimised(O(m + n)) solution:
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m, n = len(matrix), len(matrix[0])
+        i, j = 0, n - 1
+        while i < m and j >= 0:
+            if matrix[i][j] == target:
+                return True
+            elif matrix[i][j] > target:
+                j -= 1
+            else:
+                i += 1
+        return False
