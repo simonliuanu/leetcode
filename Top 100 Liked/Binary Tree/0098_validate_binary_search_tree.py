24ebf6ecfa0c494f2d5eb8f9331b02972779f662
Simon <ximeng.liu@anu.edu.au>
2025-07-13 04:44:39 +1000

    Add inorder solution of 0098

--- Top 100 Liked/Binary Tree/0098_validate_binary_search_tree.py
+++ Top 100 Liked/Binary Tree/0098_validate_binary_search_tree.py
@@ -12,3 +12,16 @@ class Solution:
             return True
         v = root.val
         return l < v < r and self.isValidBST(root.left, l, v) and self.isValidBST(root.right, v, r)
+
+# Inorder solution:
+class Solution:
+    pre = -inf
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+        if not self.isValidBST(root.left):
+            return False
+        if root.val <= self.pre:
+            return False
+        self.pre = root.val
+        return self.isValidBST(root.right)
