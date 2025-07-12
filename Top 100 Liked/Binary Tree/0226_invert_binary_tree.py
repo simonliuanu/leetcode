74544a0d05daff2b81cd2c0a3b6c3da2caca1410
Simon <ximeng.liu@anu.edu.au>
2025-07-13 01:25:06 +1000

    Add solution for 0226

---
+++ Top 100 Liked/Binary Tree/0226_invert_binary_tree.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# Recursion:
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root:
+            tmp = root.left
+            root.left = self.invertTree(root.right)
+            root.right = self.invertTree(tmp)
+        return root
+
+# Iteration:
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            node.left, node.right = node.right, node.left
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+        return root
