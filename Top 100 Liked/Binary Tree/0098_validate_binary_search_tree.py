4a04f04c875ba9cbfaded04be48b64b796f5d3b4
Simon <ximeng.liu@anu.edu.au>
2025-07-13 04:44:25 +1000

    Add preorder solution of 0098

---
+++ Top 100 Liked/Binary Tree/0098_validate_binary_search_tree.py
@@ -0,0 +1,14 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# Preorder solution:
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode], l=-inf, r=inf) -> bool:
+        if not root:
+            return True
+        v = root.val
+        return l < v < r and self.isValidBST(root.left, l, v) and self.isValidBST(root.right, v, r)
