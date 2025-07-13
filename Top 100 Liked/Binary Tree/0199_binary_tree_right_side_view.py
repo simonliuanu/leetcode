f864b729bd2db0067a985fc7dbd124bda7cb98ab
Simon <ximeng.liu@anu.edu.au>
2025-07-14 06:16:20 +1000

    Add solution for 0199

---
+++ Top 100 Liked/Binary Tree/0199_binary_tree_right_side_view.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# iteration solution(modified from level-order traversal):
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        ans = []
+        queue = deque([root])
+        while queue:
+            n = len(queue)
+            for i in range(n):
+                node = queue.popleft()
+                if i == n - 1:
+                    ans.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return ans
+
+
