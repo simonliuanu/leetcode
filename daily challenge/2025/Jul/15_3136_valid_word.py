e7fe01ffd31a079ee599acebb0c04e299ae0e120
Simon <ximeng.liu@anu.edu.au>
2025-07-15 03:29:16 +1000

    Add solution for 3136

---
+++ daily challenge/2025/Jul/15_3136_valid_word.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isValid(self, word: str) -> bool:
+        if len(word) < 3:
+            return False
+        f = [False] * 2
+        for c in word:
+            if c.isalpha():
+                f[c.lower() in "aeiou"] = True
+            else:
+                if not c.isdigit():
+                    return False
+        return all(f)
