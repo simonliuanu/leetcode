76f6f9ef3ff6d43b25c54bb45028eab31eba144c
Simon <ximeng.liu@anu.edu.au>
2025-07-20 11:49:59 +1000

    Add solution of 0739

---
+++ Top 100 Liked/Stack/0739_daily_temperatures.py
@@ -0,0 +1,10 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        ans = [0] * len(temperatures)
+        stack = []
+        for i, t in enumerate(temperatures):
+            while stack and temperatures[stack[-1]] < t:
+                j = stack.pop()
+                ans[j] = i - j
+            stack.append(i)
+        return ans
