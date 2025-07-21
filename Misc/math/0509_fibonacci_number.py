3542484bfb56ae069038d3c0c7510ee2b66f88a9
Simon <ximeng.liu@anu.edu.au>
2025-07-21 20:25:04 +1000

    Update 0509 with dp solution

--- Misc/math/0509_fibonacci_number.py
+++ Misc/math/0509_fibonacci_number.py
@@ -3,3 +3,15 @@ class Solution:
     @cache # this decorator stores previous calculated results in cache, makes this solution very fast
     def fib(self, n: int) -> int:
         return n if n < 2 else self.fib(n - 1) + self.fib(n - 2)
+
+# dp solution:
+class Solution:
+    def fib(self, n: int) -> int:
+        if n < 2:
+            return n
+        a, b = 0, 1
+        ans = 0
+        for _ in range(2, n + 1):
+            ans = a + b
+            a, b = b, ans
+        return ans
