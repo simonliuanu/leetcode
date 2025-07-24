568a15cee52bc6c8554fabce03c633067a746360
Simon <ximeng.liu@anu.edu.au>
2025-07-24 23:13:03 +1000

    Add solution for 0121

---
+++ Top 100 Liked/Greedy/0121_best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        ans = 0
+        f = prices[0]
+        for i in prices:
+            ans = max(ans, i - f)
+            f = min(i, f)
+        return ans
