550893a8312fa55564fb859d3b8baaacb4d1f638
Simon <ximeng.liu@anu.edu.au>
2025-07-31 21:30:52 +1000

    Add solution for 200

---
+++ Misc/others/200_number_of_islands.py
@@ -0,0 +1,25 @@
+# self solution:
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+
+        def bfs(row, col, i, j, grid):
+            if grid[i][j] == '1':
+                grid[i][j] = '0'
+            if i + 1 < row and grid[i + 1][j] == '1':
+                bfs(row, col, i + 1, j, grid)
+            if i - 1 >= 0 and grid[i - 1][j] == '1':
+                bfs(row, col, i - 1, j, grid)
+            if j + 1 < col and grid[i][j + 1] == '1':
+                bfs(row, col, i, j + 1, grid)
+            if j - 1 >= 0 and grid[i][j - 1] == '1':
+                bfs(row, col, i, j - 1, grid)
+
+        ans = 0
+        row = len(grid)
+        col = len(grid[0])
+        for i in range(row):
+            for j in range(col):
+                if grid[i][j] == '1':
+                    ans += 1
+                    bfs(row, col, i, j, grid)
+        return ans
