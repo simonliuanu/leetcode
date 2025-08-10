c0e72a9784a3f88d48ef8c4cbe8e964c62556aa4
Simon <ximeng.liu@anu.edu.au>
2025-08-10 18:52:17 +1000

    Add solution for 0869

---
+++ daily challenge/2025/Aug/10_0869_reordered_power_of_2.py
@@ -0,0 +1,30 @@
+# permutation solution, slow:
+class Solution:
+    def reorderedPowerOf2(self, n: int) -> bool:
+        digits = [int(d) for d in str(n)]
+        digits.sort()
+        length = len(digits)
+        nums = []
+        path = []
+        used = [False for _ in range(length)]
+
+        def dfs():
+            if len(path) == length:
+                nums.append(reduce(lambda acc, d: acc * 10 + d, path, 0))
+            for i in range(length):
+                if used[i] or (digits[i] == 0 and not path):
+                    continue
+                if i > 0 and digits[i] == digits[i - 1] and not used[i - 1]:
+                    continue
+                path.append(digits[i])
+                used[i] = True
+                dfs()
+                path.pop()
+                used[i] = False
+
+        dfs()
+        for num in nums:
+            if num & (num - 1) == 0:
+                return True
+        return False
+
