7881486b722d25975873e1b5a53b3d9231baa013
Simon <ximeng.liu@anu.edu.au>
2025-08-13 11:10:06 +1000

    Add solution for 0326

---
+++ daily challenge/2025/Aug/13_0326_power_of_three.py
@@ -0,0 +1,30 @@
+# solution 1:
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        i = 3
+        x = 0
+        while True:
+            p = i ** x
+            if p == n:
+                return True
+            if p > n:
+                break
+            x += 1
+        return False
+
+# solution 2:
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        while n > 1:
+            if n % 3 != 0:
+                return False
+            n /= 3
+        return True
+
+# double O(1) solution:
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        p = 3 ** 19
+        return n > 0 and p % n == 0
