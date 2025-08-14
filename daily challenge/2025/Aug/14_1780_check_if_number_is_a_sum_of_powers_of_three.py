a3786c2f7dae056120618d33e18d43db59532b2f
Simon <ximeng.liu@anu.edu.au>
2025-08-14 22:28:13 +1000

    Update solution of 1780

--- daily challenge/2025/Aug/14_1780_check_if_number_is_a_sum_of_powers_of_three.py
+++ daily challenge/2025/Aug/14_1780_check_if_number_is_a_sum_of_powers_of_three.py
@@ -1,3 +1,12 @@
+# solution 1:
+class Solution:
+    def checkPowersOfThree(self, n: int) -> bool:
+        while n > 0:
+            if n % 3 == 0 or n % 3 == 1:
+                n //= 3
+            else:
+                return False
+        return True
 
 # optimised:
 class Solution:
