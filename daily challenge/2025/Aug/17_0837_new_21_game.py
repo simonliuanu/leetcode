ad1b00a5d68080a62c2ac47a4a4795e0c239353c
Simon <ximeng.liu@anu.edu.au>
2025-08-17 23:53:35 +1000

    Add solution for 0837

---
+++ daily challenge/2025/Aug/17_0837_new_21_game.py
@@ -0,0 +1,12 @@
+# dp solution, out of time:
+class Solution:
+    def new21Game(self, n: int, k: int, maxPts: int) -> float:
+        dp = [0 for _ in range(k + maxPts)]
+        for i in range(k, k + maxPts):
+            if i <= n:
+                dp[i] = 1
+        for i in range(k - 1, -1, -1):
+            for j in range(1, maxPts + 1):
+                dp[i] += dp[i + j] / maxPts
+        return dp[0]
+
