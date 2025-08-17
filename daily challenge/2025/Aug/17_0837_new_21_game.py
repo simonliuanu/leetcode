c3655bae7d38f2b0ace225287970fbfe3281833a
Simon <ximeng.liu@anu.edu.au>
2025-08-18 00:09:14 +1000

    Update optimised solution for 0837

--- daily challenge/2025/Aug/17_0837_new_21_game.py
+++ daily challenge/2025/Aug/17_0837_new_21_game.py
@@ -10,3 +10,17 @@ class Solution:
                 dp[i] += dp[i + j] / maxPts
         return dp[0]
 
+# optimised solution:
+class Solution:
+    def new21Game(self, n: int, k: int, maxPts: int) -> float:
+        dp = [0 for _ in range(k + maxPts)]
+        s = 0
+        for i in range(k, k + maxPts):
+            if i <= n:
+                dp[i] = 1
+                s += 1
+        for i in range(k - 1, -1, -1):
+            dp[i] = s / maxPts
+            s = s - dp[i + maxPts] + dp[i]
+        return dp[0]
+
