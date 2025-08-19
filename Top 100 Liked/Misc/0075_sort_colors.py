8b031a8d84d0f4610e9b5cd76b9553991a6f215b
simon liu <ximeng.liu@anu.edu.au>
2025-08-19 14:28:55 +1000

    Add solution for 0075

---
+++ Top 100 Liked/Misc/0075_sort_colors.py
@@ -0,0 +1,20 @@
+# self solution:
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        l = r = 0
+        while r < n:
+            if nums[r] == 0:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+            r += 1
+        r = l
+        while r < n:
+            if nums[r] == 1:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+            r += 1
+
