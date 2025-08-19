7e77e65893aecd50ccba18e522966617cb4b38de
simon liu <ximeng.liu@anu.edu.au>
2025-08-19 14:47:07 +1000

    Update 0075 with one-pass solution

--- Top 100 Liked/Misc/0075_sort_colors.py
+++ Top 100 Liked/Misc/0075_sort_colors.py
@@ -32,3 +32,18 @@ class Solution:
                 nums[l], nums[r] = nums[r], nums[l]
                 l += 1
 
+# one-pass(Dutch national flag) solution:
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        l = m = 0
+        h = len(nums) - 1
+        while m <= h:
+            if nums[m] == 0:
+                nums[l], nums[m] = nums[m], nums[l]
+                l += 1
+                m += 1
+            elif nums[m] == 1:
+                m += 1
+            else:
+                nums[m], nums[h] = nums[h], nums[m]
+                h -= 1
