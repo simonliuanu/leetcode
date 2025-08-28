c7ef594b2c31f66e7be0edadb115b965e01c2635
Simon <ximeng.liu@anu.edu.au>
2025-08-29 03:08:34 +1000

    Add solution for 3346

---
+++ daily challenge/2025/Aug/28_3346_sort_matrix_by_diagonals.py
@@ -0,0 +1,18 @@
+class Solution:
+    def sortMatrix(self, grid: List[List[int]]) -> List[List[int]]:
+        n = len(grid)
+
+        for i in range(n):
+            tmp = [grid[i + j][j] for j in range(n - i)]
+            tmp.sort(reverse=True)
+            for j in range(n - i):
+                grid[i + j][j] = tmp[j]
+
+        for j in range(1, n):
+            tmp = [grid[i][i + j] for i in range(n - j)]
+            tmp.sort()
+            for i in range(n - j):
+                grid[i][i + j] = tmp[i]
+
+        return grid
+
