9efd14bceabc9f152dff2bb840aa786304d0887a
Simon <ximeng.liu@anu.edu.au>
2025-09-13 10:09:54 +1000

    Update solutino of 3541

--- daily challenge/2025/Sep/13_3541_find_most_frequent_vowel_and_consonant.py
+++ daily challenge/2025/Sep/13_3541_find_most_frequent_vowel_and_consonant.py
@@ -9,3 +9,16 @@ class Solution:
             else:
                 consonant[c] += 1
         return max(vowel.values(), default=0) + max(consonant.values(), default=0)
+
+# optimised solution:
+class Solution:
+    def maxFreqSum(self, s: str) -> int:
+        cnt = Counter(s)
+        chs = set('aeiou')
+        mx1 = mx2 = 0
+        for ch, v in cnt.items():
+            if ch in chs:
+                mx1 = max(mx1, v)
+            else:
+                mx2 = max(mx2, v)
+        return mx1 + mx2
