e68a275f7d084ddcbab1566952ddd5cf3b5c82e4
Simon <ximeng.liu@anu.edu.au>
2025-09-22 15:07:00 +1000

    Update solution for 3005

--- daily challenge/2025/Sep/22_3005_count_elements_with_maximum_frequency.py
+++ daily challenge/2025/Sep/22_3005_count_elements_with_maximum_frequency.py
@@ -5,3 +5,10 @@ class Solution:
             d[i] += 1
         max_freq = max(d.values())
         return sum(freq for freq in d.values() if freq == max_freq)
+
+# another solution:
+class Solution:
+    def maxFrequencyElements(self, nums: List[int]) -> int:
+        counts = Counter(nums).values()
+        m = max(counts)
+        return sum(i for i in counts if i == m)
