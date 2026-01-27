8ff8d594288bdeb694e269b192f307889bb32c9e
Simon Liu <simon@Simons-MacBook-Air.local>
2026-01-27 19:39:37 +0800

    Add solution for 0088

---
+++ Top 100 Liked/Arrays/0088_merge_sorted_array.py
@@ -0,0 +1,30 @@
+# sort method:
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        for i in range(m, m + n):
+            nums1[i] = nums2[i - m]
+        nums1.sort()
+
+# two pointers method:
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        p1, p2, p = m - 1, n - 1, m + n - 1
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] >= nums2[p2]:
+                nums1[p] = nums1[p1]
+                nums1[p1] = nums2[p2]
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            p -= 1
+        while p2 >= 0:
+            nums1[p] = nums2[p2]
+            p2 -= 1
+            p -= 1
