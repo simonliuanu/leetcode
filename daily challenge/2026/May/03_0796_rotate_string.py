0e7cd0b66a2adc156ab96a2d07ea223d1213c616
Simon Liu <simon@Simons-MacBook-Air.local>
2026-05-04 12:50:49 +0800

    Update solution

--- daily challenge/2026/May/03_0796_rotate_string.py
+++ daily challenge/2026/May/03_0796_rotate_string.py
@@ -1,3 +1,15 @@
+# smart solution:
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
         return len(s) == len(goal) and goal in s + s
+
+# simulation:
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        if len(s) != len(goal):
+            return False
+        for _ in range(len(s)):
+            s = s[1:] + s[0]
+            if s == goal:
+                return True
+        return False
